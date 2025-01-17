eda319b154e837363e1faea504de368d1fcf7092
Ketan Shinde <ketan_shinde@hotmail.com>
2025-01-17 17:59:05 +0530

    added some DevDependiencies and Partial Folder

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const PORT= 7001;
 app.set("view engine", "ejs");
 app.set("views", path.resolve('./views'));
 
-
+app.get('/', (req, res)=>{
+    res.render('homepage');
+})
 
 app.listen(PORT,()=>console.log(`Server Running On PORT: ${PORT}`));
