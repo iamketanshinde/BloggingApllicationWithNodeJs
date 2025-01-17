6d7e20980755791435ffd95865d6b9b29359a61c
Ketan Shinde <ketan_shinde@hotmail.com>
2025-01-17 17:34:33 +0530

    created PORT 7001

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("First-Commit");
+const express = require('express');
+const path = require('path');
+const app= express();
+const PORT= 7001;
+
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve('./views'));
+
+
+
+app.listen(PORT,()=>console.log(`Server Running On PORT: ${PORT}`));
