64c4a00ac7860a28c94874560d9d08f315bdc5da
Ketan Shinde <ketan_shinde@hotmail.com>
2025-01-20 17:22:48 +0530

    public.png added

--- model/userModel.js
+++ model/userModel.js
@@ -37,13 +37,17 @@ const userSchema= new Schema({
 
 userSchema.pre('save', function(next){
     const user = this;
+
     if(!user.isModified('password')) return;
+
     const salt = randomBytes(16).toString();
     const hashedPassword = createHmac('sha256', salt)
     .update(user.password)
     .digest('hex');
+
     this.salt = salt;
     this.password = hashedPassword;
+    
     next()
 })
 
