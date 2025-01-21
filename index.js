0e92c48d6e1b558ff3ba82d740efe7c061ec7790
Ketan Shinde <ketan_shinde@hotmail.com>
2025-01-21 23:10:10 +0530

    route imported in index.js file

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const path = require('path');
 const app= express();
 const PORT= 7001;
-
+const userRoute = require("./routes/userRoutes")
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve('./views'));
 
+
 app.get('/', (req, res)=>{
     res.render('homepage');
 })
+app.use('/user',userRoute);
 
 app.listen(PORT,()=>console.log(`Server Running On PORT: ${PORT}`));
