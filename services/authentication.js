2ef9c5a72e7d0bbd1a9f1b35b8b32e33b6e447fc
Ketan Shinde <ketan_shinde@hotmail.com>
2025-01-23 21:49:47 +0530

    createTokenForUser function created

--- services/authentication.js
+++ services/authentication.js
@@ -1,2 +1,15 @@
 const JWT = require("jsonwebtoken");
 const secret = "$ECRET@KEY";
+
+
+
+function createTokenForUser(user){
+    const payload={
+        _id:user._id,
+        email:user.email,
+        profileImgUrl:user.profileImgUrl,
+        role:user.role,
+    }
+    const token = JWT.sign(payload,secret)
+    return token;
+}
