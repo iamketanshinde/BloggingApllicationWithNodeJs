9b0ab69a3c48e5c1274f46ccb0297f0168251360
Ketan Shinde <ketan_shinde@hotmail.com>
2025-01-26 21:48:48 +0530

    blogSchema created

---
+++ model/blog.js
@@ -0,0 +1,29 @@
+const { Schema, model }= require("mongoose");
+
+
+const blogSchema = new Schema({
+    title: {
+        type:String,
+        required:true,
+    },
+    body: {
+        type:String,
+        required:true,
+    },
+    coverImgUrl: {
+        type:String,
+        required:false,
+    },
+    createdBy: {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+    }
+},
+{
+    timestamps:true
+});
+
+
+const Blog = model("Blog", blogSchema);
+
+module.exports= Blog;
