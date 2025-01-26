1e59533c788a53c400ca28c23be6e8ea593a205d
Ketan Shinde <ketan_shinde@hotmail.com>
2025-01-26 22:28:38 +0530

    get router created on blog.js file

--- routes/blog.js
+++ routes/blog.js
@@ -1,1 +1,11 @@
 const { Router } = require("express");
+const router  = Router();
+
+
+router.get("/addnew",(req, res)=>{
+    return res.render('addblog', {
+        user:req.user,
+    })
+})
+
+module.exports = router;
