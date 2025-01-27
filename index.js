6bae4b11c040a28b0df05381162f6ee0acbad131
Ketan Shinde <ketan_shinde@hotmail.com>
2025-01-27 23:11:45 +0530

    an img will be shown in card type an uodate is done in home.es file

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const PORT= 7001;
 
 const userRoute = require("./routes/userRoutes")
 const blogRoute = require("./routes/blog")
+const Blog  = require('./model/blog.js')
 
 
 const mongoose = require("mongoose");
@@ -15,14 +16,18 @@ const { chechForAuthenticationCookie } = require('./middleware/authenticationMid
 app.use(express.urlencoded({extended:false}));
 app.use(cookieParser());
 app.use(chechForAuthenticationCookie("token"));
+app.use(express.static(path.resolve('./public')));
+
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve('./views'));
 mongoose.connect("mongodb://localhost:27017/BlogifyApplication").then((e)=>console.log("mongodb Connected!"))
 
-app.get('/', (req, res)=>{
+app.get('/', async(req, res)=>{
+    const allblogs = await Blog.find({}); 
     res.render('homepage',{
         user:req.user,
+        blogs:allblogs,
     });
 });
 
