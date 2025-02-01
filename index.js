bed10ad25fda80468ef050c492c4536fdf4cc76b
Ketan Shinde <ketan_shinde@hotmail.com>
2025-02-01 15:37:02 +0530

    import and export  module is installed

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app= express();
-const PORT= 7001;
+const PORT= process.env.PORT || 7001;
 
 
 const userRoute = require("./routes/userRoutes")
 const blogRoute = require("./routes/blog")
 const Blog  = require('./model/blog.js')
 
-
+                                                                                                                                
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const { chechForAuthenticationCookie } = require('./middleware/authenticationMiddleware.js');
@@ -21,7 +21,10 @@ app.use(express.static(path.resolve('./public')));
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve('./views'));
-mongoose.connect("mongodb://localhost:27017/BlogifyApplication").then((e)=>console.log("mongodb Connected!"))
+
+mongoose
+.connect("mongodb://localhost:27017/BlogifyApplication")
+.then((e)=>console.log("mongodb Connected!"));
 
 app.get('/', async(req, res)=>{
     const allblogs = await Blog.find({}); 
